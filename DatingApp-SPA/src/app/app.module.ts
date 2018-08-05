08d388056bd7b0f0ed8e407ec904c3da1ca28955
Brian Walrod <brian.walrod.bg@outlook.com>
2018-08-04 20:49:46 -0400

    EOD 2018-08-04

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,7 +1,5 @@
-import { AuthGuard } from './_guards/auth.guard';
-import { appRoutes } from './routes';
-import { AlertifyService } from './_services/alertify.service';
-import { AuthService } from './_services/auth.service';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -9,12 +7,18 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { RouterModule } from '../../node_modules/@angular/router';
 
+import { UserService } from './_services/user.service';
+import { AuthGuard } from './_guards/auth.guard';
+import { appRoutes } from './routes';
+import { AlertifyService } from './_services/alertify.service';
+import { AuthService } from './_services/auth.service';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 
@@ -27,7 +31,8 @@ import { MessagesComponent } from './messages/messages.component';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent
    ],
    imports: [
       BrowserModule,
@@ -40,6 +45,7 @@ import { MessagesComponent } from './messages/messages.component';
       AuthService,
       ErrorInterceptorProvider,
       AlertifyService,
+      UserService,
       AuthGuard
    ],
    bootstrap: [
